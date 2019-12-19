7a754774f39268c6b9d4eede8ceb6066bb34ddca
umairx97 <umair.bajwa@hotmail.com>
2019-12-20 03:07:56 +0500

    add animation on hover

--- src/components/header.js
+++ src/components/header.js
@@ -5,17 +5,11 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `dodgerblue`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+    <div className="flexer" style={{ padding: 15 }}>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
